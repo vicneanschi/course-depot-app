1fcc698a9c44467c83b62276245518b302716262
Valerii Vicneanschi <vicneanschi@gmail.com>
2012-12-06 20:17:21 +0200

    Added the validations for Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, :price, :presence => true
+	validates :title, :uniqueness => true
+	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+	validates :image_url, :format => {:with => %r{\.(gif|jpg|png)$}i,
+										:message => "must be gif|jpg|png"}
 end
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
   setup do
-    @product = products(:one)
+    @product = products(:alice)
   end
 
   test "should get index" do
