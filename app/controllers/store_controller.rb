45c2af46eac39ef6fd18d008f98bec56d4c12a1f
Valerii Vicneanschi <vicneanschi@gmail.com>
2012-12-08 11:08:21 +0200

    some UI

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,5 @@
 class StoreController < ApplicationController
   def index
-	@products = Product.cheap #.all
+	@products = Product.all
   end
 end
--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,8 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+	assert_select "#main .entry"
+	assert_select "#h3", "Alice in Wonderland"
   end
 
 end
